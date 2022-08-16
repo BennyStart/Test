114bc6b62c48929a65fc8208f215fd7fb6bd8947
wangbt <wangbt@dragoninfo.com.cn>
2022-08-16 09:59:28 +0800

    1

--- fd-common-core/src/main/java/com/fardo/common/aspect/annotation/SwaggerIgnoreParameter.java
+++
@@ -1,8 +0,0 @@
-package com.fardo.common.aspect.annotation;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.METHOD, ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
-public @interface SwaggerIgnoreParameter {
-}
--- fd-common-core/src/main/java/com/fardo/common/util/security/entity/MyKeyPair.java
+++
@@ -1,9 +0,0 @@
-package com.fardo.common.util.security.entity;
-
-import lombok.Data;
-
-@Data
-public class MyKeyPair {
-    private String priKey;
-    private String pubKey;
-}
